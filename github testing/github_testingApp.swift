c4a97cb1f913624c8470ff0e3459bb823f5dd51e
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-06-18 18:21:43 +0800

    Initial Commit

---
+++ github testing/github_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_testingApp.swift
+//  github testing
+//
+//  Created by Celine Quek on 18/6/23.
+//
+
+import SwiftUI
+
+@main
+struct github_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
